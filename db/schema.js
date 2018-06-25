d880114dd06ca8d0c29031e5ce4560c308b06ead
plaifang7 <plaifang7@gmail.com>
2018-06-25 14:38:31 -0400

    schema completed and seeds currently being worked

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,56 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const ItemSchema = new Schema ({
+    itemName: {
+        type: 'String',
+        required: true, 
+    },
+    img: {
+        type: 'String'
+    },
+    checkedItem: false
+})
 
-// module.exports = 
+const ShopppingListSchema = new Schema ({
+    storeName: {
+        type: 'String'
+    },
+    listDate: {
+        type: 'Date'
+    },
+    listName: {
+        type: 'String'
+    },
+    items: [ItemSchema]
+})
+
+const UserSchema = new Schema ({
+    userName: {
+        type: 'String',
+        required: true
+    },
+    email: {
+        type: 'String',
+        required: true
+    },
+    firstName: {
+        type: 'String',
+        required: true
+    },
+    lastName: {
+        type: 'String',
+        required: true
+    },
+    shoppingList: [ShopppingListSchema]
+})
+
+const UserModel = mongoose.model('User', UserSchema)
+const ListModel = mongoose.model('List', ShopppingListSchema)
+const ItemModel = mongoose.model('Item', ItemSchema)
+
+module.exports = {
+    UserModel,
+    ListModel,
+    ItemModel
+}
