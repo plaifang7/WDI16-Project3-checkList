cd1970723cb8bf14b4ee4345ada10256d0585069
plaifang7 <plaifang7@gmail.com>
2018-06-25 00:35:14 -0400

    created react app and npm i concurrently

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI); //mongodb://localhost/checkList
 
-var app = express();
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+// If the connection throws an error
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+
+let usersRouter = require('./routes/users');
+
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
