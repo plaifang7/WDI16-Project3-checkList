d17a1bde533bc616defcfab8db750eae0bc70129
plaifang7 <plaifang7@gmail.com>
2018-06-29 12:10:59 -0400

    added form for new list and working on handlechange and handlesubmit

--- client/src/components/NewList.js
+++ client/src/components/NewList.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
 class NewList extends Component {
+  state = {
+    storeName: '',
+    listDate: '',
+    listName: ''
+  }
+  
   render() {
     return (
       <div>
-        <h1>Hi I'm Paul</h1>
+      <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="First Name"
+            type="text"
+            name="storeName"
+            value={this.state.storeName}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            placeholder="Last Name"
+            type="text"
+            name="listDate"
+            value={this.state.listDate}
+            onChange={this.handleChange}
+          />
+          <br />
+          <input
+            placeholder="E-mail"
+            type="text"
+            name="email"
+            value={this.state.listName}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
