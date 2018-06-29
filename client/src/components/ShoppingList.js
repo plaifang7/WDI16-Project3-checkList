149afbcd9667f2507d8d463880cbe5df5541c690
plaifang7 <plaifang7@gmail.com>
2018-06-29 19:10:19 -0400

    item list is showing and added a delete button, but not able to get it to work

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -5,17 +5,20 @@ class ShoppingList extends Component {
   state = {
     shoppingList: {},
     items: []
+
   }
 
   componentDidMount() {
     const userId = this.props.match.params.userId
     const listId = this.props.match.params.listId
-   
+
 
     axios.get(`/api/users/${userId}/list/${listId}`)
       .then(res => {
         this.setState({
-          shoppingList: res.data.list
+          shoppingList: res.data.list,
+          items: res.data.list.items
+
         })
         console.log(res.data)
       })
@@ -24,12 +27,47 @@ class ShoppingList extends Component {
       })
   }
 
+  deleteList = (listId) => {
+    const userId = this.props.match.params.userId
+    console.log(userId)
+
+    axios.delete(`/api/users/${userId}/list/${listId}`)
+      .then(res => {
+        this.props.history.push(`/users/${userId}`)
+      })
+  }
+
+  deleteItem = (item) => {
+    const userId = this.props.match.params.userId
+    const listId = this.props.match.params._id
+    const itemId = item._id
+
+    axios.delete(`/api/users/${userId}/list/${listId}/items/${itemId}`)
+      .then(res => {
+        this.props.history.push(`/users/${userId}/list/${itemId}`)
+      })
+
+  }
+
 
   render() {
     return (
       <div>
-        hello
+        
         <h1>{this.state.shoppingList.listName}</h1>
+        <div>
+          <button onClick={this.deleteList}>Delete List</button>
+        </div>
+          {this.state.items.map((item) => {
+            return(
+              <div key={item._id}>
+              <p>{item.itemName}</p>
+              <img src={item.img} alt={item.itemName}/>
+              <button>✓</button>
+              <button onClick={() => this.deleteItem(item._id)}>Delete Item</button>
+              </div>
+            )
+          })}
       </div>
     );
   }
