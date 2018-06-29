da1554c19fb99f93f6ddfd7280b38e2a6731ef6c
plaifang7 <plaifang7@gmail.com>
2018-06-29 15:13:24 -0400

    working on a shopping list show page

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,20 +2,36 @@ import React, { Component } from 'react';
 import axios from 'axios'
 
 class ShoppingList extends Component {
-    state = {
-        lists: []
-    }
+  state = {
+    shoppingList: {},
+    items: []
+  }
 
-    
-   
+  componentDidMount() {
+    const userId = this.props.match.params.userId
+    const listId = this.props.match.params.listId
 
-    render() {
-        return (
-            <div>
-               hello
+    axios.get(`/api/users/${userId}/list/${listId}`)
+      .then(res => {
+        this.setState({
+          shoppingList: res.data.users.shoppingList,
+          items: res.data.users.shoppingList.items
+        })
+        console.log(res.data.users.shoppingList)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+
+  render() {
+    return (
+      <div>
+        hello
             </div>
-        );
-    }
+    );
+  }
 }
 
 export default ShoppingList;
